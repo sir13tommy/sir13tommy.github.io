b7c8131fa92c8bdd014e1d04500718d9257b6361
Artem Anashchenkov <anashchenkov@g5software.com>
2018-12-28 16:57:10 +0200

    Remove redundant request on works page

--- js/dist/works.js
+++ js/dist/works.js
@@ -1,12 +1,15 @@
 (function() {
   var gameSrcKey = 'src'
   var origin = window.location.origin
+  var defaultGameSrc = origin + '/works/neo-noir'
 
   var $frame = $('.mobile iframe')
   var urlParams = new URLSearchParams(window.location.search);
   var gameSrc = origin + urlParams.get(gameSrcKey)
-  debugger
+
   if (gameSrc) {
     $frame.attr('src', gameSrc)
+  } else {
+    $frame.attr('src', defaultGameSrc)
   }
 })()
