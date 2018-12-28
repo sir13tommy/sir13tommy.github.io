fd2d4ddbdfb976d9afc5c918ed2eb08f7d616d4c
Artem Anashchenkov <anashchenkov@g5software.com>
2018-12-28 16:53:49 +0200

    Fix mobile preview in

--- js/dist/works-min.js
+++ js/dist/works-min.js
@@ -1,1 +1,1 @@
-!function(){var a=$(".mobile iframe"),r=new URLSearchParams(window.location.search).get("src");r&&a.attr("src",r)}();
+!function(){var a=window.location.origin,i=$(".mobile iframe"),o=a+new URLSearchParams(window.location.search).get("src");o&&i.attr("src",o)}();
--- js/works.js
+++ js/works.js
@@ -1,10 +1,11 @@
 (function() {
   var gameSrcKey = 'src'
+  var origin = window.location.origin
 
   var $frame = $('.mobile iframe')
   var urlParams = new URLSearchParams(window.location.search);
-  var gameSrc = urlParams.get(gameSrcKey)
-
+  var gameSrc = origin + urlParams.get(gameSrcKey)
+  
   if (gameSrc) {
     $frame.attr('src', gameSrc)
   }
